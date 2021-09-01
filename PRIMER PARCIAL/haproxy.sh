27e012a518d67f595f8af9e0937c86ab06118c1a
PerennialWarrior <88250533+PerennialWarrior@users.noreply.github.com>
2021-09-01 13:56:21 -0500

    Delete PRIMER PARCIAL directory

--- PRIMER PARCIAL/haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo sudo snap install lxd
-sudo gpasswd -a vagrant lxd
-cat /vagrant/preseed.yaml | lxd init --preseed
-sudo cp /var/snap/lxd/common/lxd/cluster.crt /vagrant/cluster.crt
-sudo sed -i 's/^/    /g' /vagrant/cluster.crt
-
